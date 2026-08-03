a42b4f31410e158db5058cf382f93935f743cbd9
crnanditha-maker <crnanditha@gmail.com>
2026-08-03 16:18:20 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution{
+
+public:
+    int trap(vector<int>& height){
+        int left=0,right=height.size()-1;
+        int lm=0,rm=0;
+        int water=0;
+        while(left<right){
+            if(height[left]<height[right]){
+                if(height[left]>=lm){
+                    lm=height[left];
+                }else{
+                    water+=lm-height[left];
+                }
+                left++;
+            }else{
+
+                if(height[right]>=rm){
+                    rm=height[right];
+                }else{
+                    water+=rm-height[right];
+                }
+                right--;
+            }
+        }
+         return water;           
+    }
+};
